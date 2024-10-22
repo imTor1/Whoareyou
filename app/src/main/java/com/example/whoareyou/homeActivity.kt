f460324dc1dbe82de319f350e23d84f7b260deae
kittipob <kittipob.jir@rmutto.ac.th>
2024-10-22 16:27:44 +0700

    home

--- app/src/main/java/com/example/whoareyou/homeActivity.kt
+++ app/src/main/java/com/example/whoareyou/homeActivity.kt
@@ -1,4 +1,24 @@
 package com.example.whoareyou
 
-class homeActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class homeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)  // ตรวจสอบว่า activity_home คือ XML ที่ใช้อยู่
+        enableEdgeToEdge()
+        // ผูกปุ่ม playButton กับการทำงาน
+        val playButton: ImageView = findViewById(R.id.playButton)
+
+        playButton.setOnClickListener {
+            // เมื่อกดปุ่ม ให้เปลี่ยนไปหน้า activity_confirm_upphoto
+            val intent = Intent(this, confrimUpphotoActivity  ::class.java)
+            startActivity(intent)
+        }
+    }
 }
