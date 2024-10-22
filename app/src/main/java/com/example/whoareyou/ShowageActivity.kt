203d9df2dacd4a21222ad066c70fbafa4da65f4e
kittipob <kittipob.jir@rmutto.ac.th>
2024-10-22 15:51:32 +0700

    remove imageview cele

--- app/src/main/java/com/example/whoareyou/ShowageActivity.kt
+++ app/src/main/java/com/example/whoareyou/ShowageActivity.kt
@@ -17,26 +17,33 @@ class ShowageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_showage)
+
+        // Adjust padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.showage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find the image view and buttons
         val imageView = findViewById<ImageView>(R.id.imageView)
         val home = findViewById<Button>(R.id.buttonupload)
+        val back = findViewById<Button>(R.id.buttonback)
+
+        // Get the image URI from the intent
         val imageUriString = intent.getStringExtra("imageUri")
         val imageUri = Uri.parse(imageUriString)
         imageView.setImageURI(imageUri)
 
-        val back = findViewById<Button>(R.id.buttonback)
+        // Set listener for back button to finish activity
         back.setOnClickListener {
             finish()
         }
+
+        // Set listener for upload button to navigate to AddphotoActivity
         home.setOnClickListener {
             val intent = Intent(this, AddphotoActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
